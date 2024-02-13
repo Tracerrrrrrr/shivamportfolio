44a48654951469cc3402a6a7645f34f966dc83e8
Shivam <shivamuoadhyay4@gmail.com>
2024-02-13 17:14:41 +0530

    first commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
+const About = () => {
+  return (
+<>
+<div style={{marginTop:"2rem"}}> 
+<p style={{color:'purple'}}>
+
+I am passionate software engineer with 1.4 years of experience in the IT industry, I have worked extensively with multiple backend and frontend technologies, including Android development. I am a quick learner and adaptable by nature, capable of working under pressure and delivering projects within given timeframes.
+
+</p>
+</div>
+</>
+  )
+}
+
+export default About
